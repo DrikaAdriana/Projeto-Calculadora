dd36ceff5babdd4e27f203f95b8b5d9a33060571
Adriana-Drika <nmdrika@gmail.com>
2022-08-29 22:07:54 -0300

    iniciando projeto calculadora , irei afinar detalhes

---
+++ calculadora.js
@@ -0,0 +1,13 @@
+let valor;
+let resultado;
+function botao(num) {
+  valor = document.calc.visor.value += num;
+}
+function limpar() {
+  document.calc.visor.value = '';
+}
+function calcular() {
+   resultado= eval(valor)
+   document.calc.visor.value = resultado.toLocaleString('pt-br')
+}
+
